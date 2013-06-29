f5d467da65913015033eba2f19f6858b14bb24ca
Jordan Goldstein <mrjordangoldstein@gmail.com>
2013-06-29 18:56:11 -0400

    Add standard app level constants

--- lib/roulettebot.rb
+++ lib/roulettebot.rb
@@ -1,5 +1,7 @@
 require "roulettebot/version"
 
 module Roulettebot
-  # Your code goes here...
+  ROOTDIR     = File.expand_path(File.dirname(__FILE__) + '/..')
+  ENVIRONMENT ||= (ENV["ROULETTEBOT_ENV"] || "development")
+  CONFIG_PATH ||= ROOTDIR + "config/app.yml"
 end
