a4bc763b90cde1be39e3447bf2836c9cb4323113
Jordan Goldstein <mrjordangoldstein@gmail.com>
2013-06-29 19:24:46 -0400

    Fix directory issues in roulettebot.rb

--- lib/roulettebot.rb
+++ lib/roulettebot.rb
@@ -1,7 +1,10 @@
+libdir = File.dirname(__FILE__)
+$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
+
 require "roulettebot/version"
 
 module Roulettebot
   ROOTDIR     = File.expand_path(File.dirname(__FILE__) + '/..')
   ENVIRONMENT ||= (ENV["ROULETTEBOT_ENV"] || "development")
-  CONFIG_PATH ||= ROOTDIR + "config/app.yml"
+  CONFIG_PATH ||= ROOTDIR + "/config/app.yml"
 end
