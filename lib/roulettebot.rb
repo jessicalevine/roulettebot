0c3631f406752019531ec2e0acce0b91a2cc4e12
Jordan Goldstein <mrjordangoldstein@gmail.com>
2013-06-29 19:44:26 -0400

    Load the config in roulettebot.rb

--- lib/roulettebot.rb
+++ lib/roulettebot.rb
@@ -8,4 +8,6 @@ module Roulettebot
   ROOTDIR     = File.expand_path(File.dirname(__FILE__) + '/..')
   ENVIRONMENT ||= (ENV["ROULETTEBOT_ENV"] || "development")
   CONFIG_PATH ||= ROOTDIR + "/config/app.yml"
+
+  config = Config.load
 end
